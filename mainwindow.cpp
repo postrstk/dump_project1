286fbec733e9f851a2a12da8022cc341dca620b4
Your Name <ignatievanton1996@gmail.com>
2020-03-26 14:14:47 +0300

    add some func

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,6 @@
 #include "mainwindow.hpp"
 #include "./ui_mainwindow.h"
-
+#include <iostream>
 #include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
@@ -15,6 +15,8 @@ MainWindow::MainWindow(QWidget *parent)
     this->outputs.push_back(ui->centralwidget->findChild<QLabel*>("output_"+QString::number(i)));
     this->inputs.push_back(ui->centralwidget->findChild<QLineEdit*>("input_"+QString::number(i)));
   }
+  writeData(0,12.5);
+  readData();
 //  testConnect();
 
 }
@@ -71,8 +73,228 @@ std::string MainWindow::double2hex(double d)
 
 void MainWindow::readData()
 {
+    QString command="ST-LINK_CLI.exe -c UR -Dump ";
+
+    process = new QProcess(this);
+    process->start("cmd.exe",QStringList()<<"/c "+command + addrRead.addr1 + " 8 " + nameF.addr1);
+    if(process->waitForFinished())
+    {
+        qDebug() <<"read";
+    }
+    process->waitForFinished(-1);
+    qDebug() << "Finish read ";
+
+    process->start("cmd.exe",QStringList()<<"/c "+command + addrRead.addr2 + " 8 " + nameF.addr2);
+    if(process->waitForFinished())
+    {
+        qDebug() <<"read";
+    }
+    process->waitForFinished(-1);
+    qDebug() << "Finish read ";
+
+    process->start("cmd.exe",QStringList()<<"/c "+command + addrRead.addr3 + " 8 " + nameF.addr3);
+    if(process->waitForFinished())
+    {
+        qDebug() <<"read";
+    }
+    process->waitForFinished(-1);
+    qDebug() << "Finish read ";
+
+    process->start("cmd.exe",QStringList()<<"/c "+command + addrRead.addr4 + " 8 " + nameF.addr4);
+    if(process->waitForFinished())
+    {
+        qDebug() <<"read";
+    }
+    process->waitForFinished(-1);
+    qDebug() << "Finish read ";
+
+    process->start("cmd.exe",QStringList()<<"/c "+command + addrRead.addr5 + " 8 " + nameF.addr5);
+    if(process->waitForFinished())
+    {
+        qDebug() <<"read";
+    }
+    process->waitForFinished(-1);
+    qDebug() << "Finish read ";
+
+    process->start("cmd.exe",QStringList()<<"/c "+command + addrRead.addr6 + " 8 " + nameF.addr6);
+    if(process->waitForFinished())
+    {
+        qDebug() <<"read";
+    }
+    process->waitForFinished(-1);
+    qDebug() << "Finish read ";
+
+    process->start("cmd.exe",QStringList()<<"/c "+command + addrRead.addr7 + " 8 " + nameF.addr7);
+    if(process->waitForFinished())
+    {
+        qDebug() <<"read";
+    }
+    process->waitForFinished(-1);
+    qDebug() << "Finish read ";
+
+    process->start("cmd.exe",QStringList()<<"/c "+command + addrRead.addr8 + " 8 " + nameF.addr8);
+    if(process->waitForFinished())
+    {
+        qDebug() <<"read";
+    }
+    process->waitForFinished(-1);
+    qDebug() << "Finish read ";
+
+    process->start("cmd.exe",QStringList()<<"/c "+command + addrRead.addr9 + " 8 " + nameF.addr9);
+    if(process->waitForFinished())
+    {
+        qDebug() <<"read";
+    }
+    process->waitForFinished(-1);
+    qDebug() << "Finish read ";
+
+    process->start("cmd.exe",QStringList()<<"/c "+command + addrRead.addr10 + " 8 " + nameF.addr10);
+    if(process->waitForFinished())
+    {
+        qDebug() <<"read";
+    }
+    process->waitForFinished(-1);
+    qDebug() << "Finish read ";
+
+    process->start("cmd.exe",QStringList()<<"/c "+command + addrRead.addr11 + " 8 " + nameF.addr11);
+    if(process->waitForFinished())
+    {
+        qDebug() <<"read";
+    }
+    process->waitForFinished(-1);
+    qDebug() << "Finish read ";
+
+    process->start("cmd.exe",QStringList()<<"/c "+command + addrRead.addr12 + " 8 " + nameF.addr12);
+    if(process->waitForFinished())
+    {
+        qDebug() <<"read";
+    }
+    process->waitForFinished(-1);
+    qDebug() << "Finish read ";
+
+    process->start("cmd.exe",QStringList()<<"/c "+command + addrRead.addr13 + " 8 " + nameF.addr13);
+    if(process->waitForFinished())
+    {
+        qDebug() <<"read";
+    }
+    process->waitForFinished(-1);
+    qDebug() << "Finish read ";
+
+    process->start("cmd.exe",QStringList()<<"/c "+command + addrRead.addr14 + " 8 " + nameF.addr14);
+    if(process->waitForFinished())
+    {
+        qDebug() <<"read";
+    }
+    process->waitForFinished(-1);
+    qDebug() << "Finish read ";
 
 }
+
+void MainWindow::writeData(int num, double data)
+{
+    std::string hex = double2hex(data);
+    qDebug() << QString::fromUtf8(hex.c_str());
+    std::string hexS = hex;
+
+    std::string secondHex = hex.erase(8);
+    std::string firstHex = hexS.erase(0,8);
+    QString comandF;
+    QString comandS;
+    switch (num) {
+    case 0:
+        comandF="ST-LINK_CLI.exe -c UR -w32 "+adr1.addr1+" "+"0x"+QString::fromUtf8(firstHex.c_str());
+        comandS="ST-LINK_CLI.exe -c UR -w32 "+adr2.addr1+" "+"0x"+QString::fromUtf8(secondHex.c_str());
+        qDebug() << comandF;
+        qDebug() << comandS;
+        break;
+    case 1:
+        comandF="ST-LINK_CLI.exe -c UR -w32 "+adr1.addr2+" "+"0x"+QString::fromUtf8(firstHex.c_str());
+        comandS="ST-LINK_CLI.exe -c UR -w32 "+adr2.addr2+" "+"0x"+QString::fromUtf8(secondHex.c_str());
+        break;
+    case 2:
+        comandF="ST-LINK_CLI.exe -c UR -w32 "+adr1.addr3+" "+QString::fromUtf8(firstHex.c_str());
+        comandS="ST-LINK_CLI.exe -c UR -w32 "+adr2.addr3+" "+QString::fromUtf8(secondHex.c_str());
+        break;
+
+    case 3:
+        comandF="ST-LINK_CLI.exe -c UR -w32 "+adr1.addr4+" "+QString::fromUtf8(firstHex.c_str());
+        comandS="ST-LINK_CLI.exe -c UR -w32 "+adr2.addr4+" "+QString::fromUtf8(secondHex.c_str());
+        break;
+
+    case 4:
+        comandF="ST-LINK_CLI.exe -c UR -w32 "+adr1.addr5+" "+QString::fromUtf8(firstHex.c_str());
+        comandS="ST-LINK_CLI.exe -c UR -w32 "+adr2.addr5+" "+QString::fromUtf8(secondHex.c_str());
+        break;
+
+    case 5:
+        comandF="ST-LINK_CLI.exe -c UR -w32 "+adr1.addr6+" "+QString::fromUtf8(firstHex.c_str());
+        comandS="ST-LINK_CLI.exe -c UR -w32 "+adr2.addr6+" "+QString::fromUtf8(secondHex.c_str());
+        break;
+
+    case 6:
+        comandF="ST-LINK_CLI.exe -c UR -w32 "+adr1.addr7+" "+QString::fromUtf8(firstHex.c_str());
+        comandS="ST-LINK_CLI.exe -c UR -w32 "+adr2.addr7+" "+QString::fromUtf8(secondHex.c_str());
+        break;
+
+    case 7:
+        comandF="ST-LINK_CLI.exe -c UR -w32 "+adr1.addr8+" "+QString::fromUtf8(firstHex.c_str());
+        comandS="ST-LINK_CLI.exe -c UR -w32 "+adr2.addr8+" "+QString::fromUtf8(secondHex.c_str());
+        break;
+
+    case 8:
+        comandF="ST-LINK_CLI.exe -c UR -w32 "+adr1.addr9+" "+QString::fromUtf8(firstHex.c_str());
+        comandS="ST-LINK_CLI.exe -c UR -w32 "+adr2.addr9+" "+QString::fromUtf8(secondHex.c_str());
+        break;
+
+    case 9:
+        comandF="ST-LINK_CLI.exe -c UR -w32 "+adr1.addr10+" "+QString::fromUtf8(firstHex.c_str());
+        comandS="ST-LINK_CLI.exe -c UR -w32 "+adr2.addr10+" "+QString::fromUtf8(secondHex.c_str());
+        break;
+
+    case 10:
+        comandF="ST-LINK_CLI.exe -c UR -w32 "+adr1.addr11+" "+QString::fromUtf8(firstHex.c_str());
+        comandS="ST-LINK_CLI.exe -c UR -w32 "+adr2.addr11+" "+QString::fromUtf8(secondHex.c_str());
+        break;
+
+    case 11:
+        comandF="ST-LINK_CLI.exe -c UR -w32 "+adr1.addr12+" "+QString::fromUtf8(firstHex.c_str());
+        comandS="ST-LINK_CLI.exe -c UR -w32 "+adr2.addr12+" "+QString::fromUtf8(secondHex.c_str());
+        break;
+
+    case 12:
+        comandF="ST-LINK_CLI.exe -c UR -w32 "+adr1.addr13+" "+QString::fromUtf8(firstHex.c_str());
+        comandS="ST-LINK_CLI.exe -c UR -w32 "+adr2.addr13+" "+QString::fromUtf8(secondHex.c_str());
+        break;
+
+    case 13:
+        comandF="ST-LINK_CLI.exe -c UR -w32 "+adr1.addr14+" "+QString::fromUtf8(firstHex.c_str());
+        comandS="ST-LINK_CLI.exe -c UR -w32 "+adr2.addr14+" "+QString::fromUtf8(secondHex.c_str());
+        break;
+
+    default:
+        return;
+        break;
+    }
+
+    process = new QProcess(this);
+    process->start("cmd.exe",QStringList()<<"/c "+comandF);
+    if(process->waitForFinished())
+    {
+        qDebug() <<"Start command "+ comandF;
+    }
+    process->waitForFinished(-1);
+    qDebug() << "Finish command "+comandF;
+
+    process->start("cmd.exe",QStringList()<<"/c "+comandS);
+    if(process->waitForFinished())
+    {
+        qDebug() <<"Start command "+ comandS;
+    }
+    process->waitForFinished(-1);
+    qDebug()<<"Finish command "+comandS;
+
+}
+
 
 bool MainWindow::testConnect()
 {
@@ -83,6 +305,7 @@ bool MainWindow::testConnect()
     }
     process->waitForFinished(-1);
         qDebug() << "finish";
+    delete process;
 
 //    process->start("ping","mail.ru > test.txt",QProcess::ReadOnly);
 }
--- mainwindow.hpp
+++ mainwindow.hpp
@@ -9,7 +9,41 @@
 #include <QLineEdit>
 #include <QProcess>
 
+struct nameFile
+{
+    QString addr1 = "1.hex";
+    QString addr2 = "2.hex";
+    QString addr3 = "3.hex";
+    QString addr4 = "4.hex";
+    QString addr5 = "5.hex";
+    QString addr6 = "6.hex";
+    QString addr7 = "7.hex";
+    QString addr8 = "8.hex";
+    QString addr9 = "9.hex";
+    QString addr10 = "10.hex";
+    QString addr11 = "11.hex";
+    QString addr12 = "12.hex";
+    QString addr13 = "13.hex";
+    QString addr14 = "14.hex";
+};
 
+struct addrRead
+{
+    QString addr1 = "0x08080100";
+    QString addr2 = "0x08080108";
+    QString addr3 = "0x08080110";
+    QString addr4 = "0x08080118";
+    QString addr5 = "0x08080120";
+    QString addr6 = "0x08080128";
+    QString addr7 = "0x08080130";
+    QString addr8 = "0x08080138";
+    QString addr9 = "0x08080140";
+    QString addr10 = "0x08080148";
+    QString addr11 = "0x08080150";
+    QString addr12 = "0x08080158";
+    QString addr13 = "0x08080160";
+    QString addr14 = "0x08080168";
+};
 
 struct addr
 {
@@ -72,6 +106,7 @@ private:
   double hex2double(const std::string& hex);
   std::string double2hex(double d);
   void readData();
+  void writeData(int num, double data);
   bool testConnect();
 public:
     QString getCorrectDataFromHex(const QString& filename);
@@ -81,5 +116,9 @@ private:
   QList<QLineEdit*> inputs;
   Ui::MainWindow *ui;
   QProcess *process;
+  addr adr1;
+  addrSec adr2;
+  addrRead addrRead;
+  nameFile nameF;
 };
 #endif // MAINWINDOW_HPP
